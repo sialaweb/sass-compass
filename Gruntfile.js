165a468e9d68bd221e55ccae8e593d7b7a87cf34
ydpire <davidpire29@gmail.com>
2015-06-09 00:19:53 -0430

    grunt file
    pruebas de Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+
+	grunt.initConfig({
+
+			uglify: {
+				my_target: {
+					files: {
+						'_/js/script.js': ['_/components/js/*.js']
+					}  // files
+				}  // target
+			} // uglify
+
+		}) // initConfig
+	}  //   exports
