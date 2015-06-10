a573b16ef9ab746ac9456a7159b214ad12c8619c
ydpire <davidpire29@gmail.com>
2015-06-09 22:35:55 -0430

    sass compass rb
    ya estamos en la mejor parte del curso ya colocamos un framework y
    workflow especifico para el proyecto que puede ser uno de los mejores
    flujos de trabajo que haya visto, cuidado sino el mejor

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-compass");
 
 	grunt.initConfig({
 
@@ -13,16 +14,30 @@ module.exports = function(grunt) {
 				}  // target
 			}, // uglify
 
+			compass: {
+				dev: {
+					options: {
+						config: 'config.rb'
+					} // options
+				}  // dev
+			}, //compass
+
 			watch: {
+
 				options: {livereload: true},
 				scripts: {
 				files: ['_/components/js/*.js'],
 				tasks: ['uglify']
-				},  // scripts
+				},  // scripts watch
 
 				html: {
 					files: ['*.html']
-				}
+				}, //html watch
+
+				sass: {
+					files: ['_/components/sass/*.scss'],
+					tasks: ['compass:dev']
+				},  //sass watch
 			}
 
 		}) // initConfig
