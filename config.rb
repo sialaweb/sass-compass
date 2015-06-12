4cbb22d362dfe755b74b45041e595534a67e326c
ydpire <davidpire29@gmail.com>
2015-06-11 23:29:15 -0430

    ultimos dias ya esta casi lista
    creacion de los partials scss...

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
-require 'sass-globbing'
+# require 'sass-globbing'
 require 'susy'
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
 output_style = :nested
 sourceComments = false
+sourcemap = true
